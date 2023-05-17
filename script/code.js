16814791f4e152eaf370b8489eb22f8b2952d905
Mongy <mongidlanjwa@gmail.com>
2023-05-17 15:22:38 +0200

    day 3

---
+++ script/code.js
@@ -0,0 +1,16 @@
+let myVariable = "Mongikazi"
+console.log(typeof myVariable);
+
+ myVariable = 890
+console.log(typeof myVariable);
+
+myVariable = true
+console.log(typeof myVariable);
+
+myVariable = undefined
+console.log(typeof myVariable);
+
+myVariable = null
+console.log(typeof myVariable);
+
+
